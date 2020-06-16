4c0591509a5622564a9510af0a90236d23914e99
gabe chavez <gabe.chavez77@gmail.com>
2020-06-15 20:19:52 -1000

    step 2

--- oop1-construct.js
+++ oop1-construct.js
@@ -19,117 +19,54 @@ const reason = new WhyClass(
 console.log(reason.explain());
 console.log(reason.pieces());
 
-//
+//your code here...
+
 /*2. You are exploring the rainforests of the Amazon.  You have observed many different types of wildlife on your excursion.  To jog your memory, some different types of wildlife that you observed are found in the following URL : https://www.ietravel.com/blog/amazon-rainforest-animals-beginners-guide-21-species.  Feel free to use other resources online if you wish.
 
 Create a class that can categorize at least 6 of the creatures that you recall observing the Amazon.  Each creature should have 3 unique characteristics, and 2 unique actions that it can do. */
 
-//your code here...
-
-/*3. Convert the following Object Literals into a general class called Shape, and instantiate the class to generate the object literals
- */
-
-class Shape {
-  //your code here...
-}
-
-const triangle = {
-  name: "triangle",
-  sides: [4, 7, 7], //lengths of each side
-  base: null,
-  height: null, //determine this height using basic geometry.  You may need to research this one.
-  calcArea: function () {
-    console.log(
-      `${this.name}'s area is calculated to be : ${
-        0.5 * this.base * this.height
-      }`
-    );
-  },
-  calcPerimeter: function () {
-    console.log(
-      `${this.name}'s perimeter is calculated to be : ${
-        this.base + (this.sides[1] + this.sides[2])
-      }`
-    );
-  },
-};
-
-triangle.base = triangle.sides[0]; //base is the 1st side in the triangle.sides array.
-triangle.height = 6.71; //determine the height using basic geometry.  How do you calculate the height of a triangle with 2 equal sides?  If you use the formula to caluclate the height, this is a bonus.  If you hard code the correct value here, that will be sufficient, but no bonus :(
-
-console.log(triangle);
-triangle.calcArea();
-triangle.calcPerimeter();
-
-const rectangle = {
-  name: "rectangle",
-  sides: 4,
-  length: 2,
-  width: 5,
-  calcArea: function () {
-    console.log(
-      `${this.name}'s area is calculated to be : ${this.length * this.width}`
-    );
-  },
-  calcPerimeter: function () {
-    console.log(
-      `${this.name}'s perimeter is calculated to be : ${
-        2 * this.length + 2 * this.width
-      }`
-    );
-  },
-};
-
-console.log(rectangle);
-rectangle.calcArea();
-rectangle.calcPerimeter();
-
-const circle = {
-  name: "circle",
-  sides: 1,
-  radius: 5,
-  calcArea: function () {
-    console.log(
-      `${this.name}'s area is calculated to be : ${(
-        Math.PI *
-        this.radius ** 2
-      ).toFixed(2)}`
-    );
-  },
-  calcCircumference: function () {
-    console.log(
-      `${this.name}'s circumference is calculated to be : ${(
-        2 *
-        Math.PI *
-        this.radius
-      ).toFixed(2)}`
-    );
-  },
-};
-
-console.log(circle);
-circle.calcCircumference();
-circle.calcArea();
-
-/*********************************************** 
-Bonus Exercises:
-
-//4. Create three more instances of the Shape class for a pentagon, hexagon, and an octagon.  Add in  comments explaining the formulas for calculating the perimeter and the areas of these 3 shapes.  Also, as noted above, if you use the formula to calculate the height of the triangle instead of hard coding it, you will also obtain the bonus for this exercise.  The dimensions for the triangle are already provided for you in the triangle object literal.
-
-
-//5. Below is a class Earth.  It is instantiated with a earth 'instance'.  Because there is only 1 known earth, we don't want multiple instances of earth.  Modify the class so the properties are usuable WITHOUT instantiating the class.
-
-*/
-
-class Earth {
-  name;
-  planetNum; //distance from the sun.  1-mercury, 2-venus, 3-earth, ...
-
-  constructor(name, num) {
+class Wildlife {
+  constructor(name, mammal, color, actions) {
     this.name = name;
-    this.planetNum = num;
+    this.mammal = mammal;
+    this.color = color;
+    this.actions = actions;
   }
 }
+const sloth = new Wildlife("sloth", true, "brown", [
+  "climb and hang from trees",
+  "move extremely slow",
+]);
+console.log(sloth);
 
-const earth = new Earth("earth", 3);
-console.log(earth);
+const boaConstrictor = new Wildlife("boa constrictor", false, "grey", [
+  "slither and crawl",
+  "squeeze its prey",
+]);
+console.log(boaConstrictor);
+
+const capybara = new Wildlife("capybara", true, "ash brown", [
+  "eat grass",
+  "float in water",
+]);
+console.log(capybara);
+
+const jaguar = new Wildlife("jaguar", true, "spotted golden", [
+  "sprint up to 35 mph",
+  "hunt its prey",
+]);
+console.log(jaguar);
+
+const hyacinthMacaw = new Wildlife("hyacinth macaw", false, "black", [
+  "fly",
+  "break coconuts with its beak",
+]);
+console.log(hyacinthMacaw);
+
+const poisonDartFrog = new Wildlife("poison dart frog", false, "orange", [
+  "hop and croak",
+  "poison its attackers",
+]);
+console.log(poisonDartFrog);
+
+//your code here...
